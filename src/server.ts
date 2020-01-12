5e12b1e74d20ac0b9f07d6262630e6f530cfd4e6
Beomjun Gil <beomjun.gil@scatterlab.co.kr>
2020-01-12 16:27:07 +0900

    chore: init commit

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,42 @@
 import Express from 'express';
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import mongoose from 'mongoose';
 
-const app = Express();
-const port = process.env.PORT || 3000;
+const mongoServer = new MongoMemoryServer();
+mongoose.Promise = Promise;
 
-app.use("*", (req, res) =>{res.send("<h1>Testing server</h1>")})
+(async () => {
+    const dburi = await mongoServer.getUri();
+    const dbPort = await mongoServer.getPort();
+    const dbPath = await mongoServer.getDbPath();
+    const dbName = await mongoServer.getDbName();
 
-app.listen(port, () => {
-    console.log(`Server stared with ${port} port.`);
-});
+    const app = Express();
+    const port = process.env.PORT || 3000;
+
+    const mongooseOpts = {
+        autoReconnect: true,
+        reconnectTries: Number.MAX_VALUE,
+        reconnectInterval: 1000,
+    };
+
+    mongoose.connect(dburi, mongooseOpts);
+
+    mongoose.connection.on('error', (e) => {
+        if (e.message.code === 'ETIMEDOUT') {
+            console.log(e);
+            mongoose.connect(dburi, mongooseOpts);
+        }
+        console.log(e);
+    });
+
+    mongoose.connection.once('open', () => {
+        console.log(`MongoDB successfully connected to ${dburi}`);
+    });
+
+    app.use("*", (req, res) => { res.send("<h1>Testing server</h1>") })
+
+    app.listen(port, () => {
+        console.log(`Server stared with ${port} port.`);
+    });
+})()
