69e554ccc6abfe0e2ebde2d843eb98f6986d8845
Beomjun Gil <gil@beomjun.kr>
2020-01-12 23:31:53 +0900

    refactor: replace todo.title to todo.name

--- src/models/todo.ts
+++ src/models/todo.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const todoSchema = new mongoose.Schema(
     {
-        title: { type: String, required: true },
+        name: { type: String, required: true },
         isFinished: { type: Boolean, default: false }
     },
     {
