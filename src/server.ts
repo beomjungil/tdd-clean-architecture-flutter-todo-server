7794101e4077cbeaba5de96314d162fefbf168f2
Beomjun Gil <beomjun.gil@scatterlab.co.kr>
2020-01-12 16:02:52 +0900

    chore: init commit

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import Express from 'express';
+
+const app = Express();
+const port = process.env.port || 3000;
+
+app.use("*",(req, res) =>{res.send("<h1>Testing server</h1>")})
+
+app.listen(port, () => {
+    console.log(`Server stared with ${port} port.`);
+});
